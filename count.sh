1f8fbe316bf7dad555b7075c18c00e281de54aa2
ahalsnad <halsna01@gw.uni-passau.de>
2017-08-16 17:24:16 +0200

    counting particular extension

--- count.sh
+++ count.sh
@@ -1,14 +1,21 @@
-#This is a script to count the number of files according to each extension 
+#This is a script to count the number of files according to each extension or to count # of files of particular extension
 <<COMMENT1
 	===================================================================
 	Author: Avinash Halsnad
 	Usage: 1. Make this script executable (chmod 755 <script.sh>)
 		   2. Run this script as ./script.sh <location> 
 				Example: ./script.sh /home/vintux/
+			3. To count just one particular extension use ./script.sh <location> <extension>
 COMMENT1
 
 #!/bin/bash
 loc=$1
+ext=$2
+if [ $2 ]; then
+     count=`ls -la $1 | grep "$2" | wc -l`
+	 echo -e "\033[0m Number of \t \033[0;32m$2\033[0m \t:\t \033[0;32m$count"
+	 exit
+fi
 search=0
 declare -A arr
 for i in `ls -l $loc| grep ^-|awk '{print $9}'|grep '\.'`; do
@@ -45,6 +52,6 @@ until [ $rounds -lt 0 ]; do
   done
 	((max--))
 	((rounds--))
-  continue
+	continue
 done
 
